30c74e798fe221ac5b6e9789894c5af0b2dc397c
ShigrafS <shigrafsalik@proton.me>
2026-01-31 19:32:43 +0530

    test(lints): add tests for pr_missing_newline

---
+++ tests/ui/pr_missing_newline.rs
@@ -0,0 +1,22 @@
+// check-pass
+
+#![feature(register_tool)]
+#![register_tool(klint)]
+
+#[macro_export]
+macro_rules! pr_info {
+    ($($arg:tt)*) => {};
+}
+
+#[macro_export]
+macro_rules! pr_cont {
+    ($($arg:tt)*) => {};
+}
+
+fn main() {
+    pr_info!("hello"); //~ WARN pr_* logging calls should end with a trailing "\n"
+    pr_info!("hello\n");
+    pr_cont!("hello");
+    pr_info!("hello {}", "world"); //~ WARN pr_* logging calls should end with a trailing "\n"
+    pr_info!("hello {}\n", "world");
+}
