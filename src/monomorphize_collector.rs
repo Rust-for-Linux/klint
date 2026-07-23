8038bf669fa7146e086abc74a24d8746f7feac51
Gary Guo <gary@garyguo.net>
2026-07-23 19:02:13 +0100

    fix(upstream): Limit is moved

--- src/monomorphize_collector.rs
+++ src/monomorphize_collector.rs
@@ -9,6 +9,7 @@
 // * Due to the above reasons, `InliningMap` is renamed to `AccessMap`.
 // * `Spanned<MonoItem>` is returned in `AccessMap` instead of just `MonoItem`.
 
+use rustc_data_structures::Limit;
 use rustc_data_structures::fx::{FxHashMap, FxIndexMap};
 use rustc_data_structures::sync::{Lock, par_for_each_in};
 use rustc_data_structures::unord::UnordSet;
@@ -17,7 +18,6 @@ use rustc_hir::attrs::InlineAttr;
 use rustc_hir::def::DefKind;
 use rustc_hir::def_id::{DefId, DefIdMap, LocalDefId};
 use rustc_hir::lang_items::LangItem;
-use rustc_hir::limit::Limit;
 use rustc_middle::middle::codegen_fn_attrs::CodegenFnAttrFlags;
 use rustc_middle::mir::interpret::AllocId;
 use rustc_middle::mir::interpret::{ErrorHandled, GlobalAlloc, Scalar};
